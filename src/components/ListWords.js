b5159ab2c999f32ece114fd29a5d7040cc32ee97
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-11 14:37:02 +0700

    list

--- src/components/ListWords.js
+++ src/components/ListWords.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
+const Word = ({ spot }) => (
+    <div>
+        <h3>{spot.en}</h3>
+        <p>{spot.vn}</p>
+    </div>
+);
+
 class ListWords extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            words: [
+                { en: 'one', vn: 'mot' },
+                { en: 'two', vn: 'hai' },
+                { en: 'three', vn: 'ba' },
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 List Words Component
+                { this.state.words.map(word => <Word key={word.en} spot={word} />) }
             </div>
         );
     }
