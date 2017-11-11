f7bde530d5b400c5d9e65e51c1daab8761323497
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-11 10:56:36 +0700

    xong

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-
+import Word from './components/Word';
 // function component
-// class component
-// function Word(props) {
-//   return (
-//     <div>
-//       <h3 style={{ color: props.color }}>{props.en}</h3>
-//       <p>{props.vn}</p>
-//     </div>
-//   )
-// }
+  // class component
+  // function Word(props) {
+  //   return (
+  // <div>
+  //   <h3 style={{ color: props.color }}>{props.en}</h3>
+  //   <p>{props.vn}</p>
+  // </div>
+  //   )
+  // }
 
-class Word extends Component {
-  render() {
-    return (
-      <div>
-        <h3 style={{ color: this.props.color }}>{this.props.en}</h3>
-        <p>{this.props.vn}</p>
-      </div>
-    );
-  }
-}
+  // const Word = (props) => (
+  //   <div>
+  //     <h3 style={{ color: props.color }}>{props.en}</h3>
+  //     <p>{props.vn}</p>
+  //   </div>
+  // );
+
+  // class Word extends Component {
+  //   render() {
+  //     return (
+  //       <div>
+  //         <h3 style={{ color: this.props.color }}>{this.props.en}</h3>
+  //         <p>{this.props.vn}</p>
+  //       </div>
+  //     );
+  //   }
+// }
 
 class App extends Component {
   render() {
---
+++ src/components/Word.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// const Word = (props) => (
+    //     <div>
+    //         <h3 style={{ color: props.color }}>{props.en}</h3>
+    //         <p>{props.vn}</p>
+    //     </div>
+    // );
+// export default Word;
+class Word extends Component {
+    render() {
+        return (
+            <div>
+                <h3 style={{ color: this.props.color }}>{this.props.en}</h3>
+                <p>{this.props.vn}</p>
+            </div>
+        );
+    }
+}
+export default Word;
