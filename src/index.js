d2174313e7f16940350c267a85e9f0104e7b3e6d
Van Pho Nguyen <vanpho02@gmail.com>
2017-12-09 15:59:14 +0700

    ?

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+// khoaphamtraining@gmail.com
+// Chu de: DO AN REACT NATIVE 0411
+// Ten do an, ten
+// Mo ta
+// Database
+// Code server
+// Github
+// Video app
